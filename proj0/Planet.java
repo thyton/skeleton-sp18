b3758a4d447dea7ef0c1bccb6b242a3d52fd96dc
Thy Ton <maithytonn@gmail.com>
2019-02-08 14:49:51 -0800

    Finished animation of moving planets

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -86,4 +86,10 @@ public class Planet {
         xxPos += xxVel*dt;
         yyPos += yyVel*dt;
     }
+
+    public void draw()
+    {
+        StdDraw.picture(xxPos, yyPos, imgFileName);
+        StdDraw.show();
+    }
 };
