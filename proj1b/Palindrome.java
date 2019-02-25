84d759419c2aee7f8a450b4b3b2bde39725c80f5
Thy Ton <maithytonn@gmail.com>
2019-02-25 15:14:31 -0800

    added private wordToCharDeque

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.DelayQueue;
 
 public class Palindrome {
-    public Deque<String> wordToDeque(String word) {
+    private Deque<String> wordToDeque(String word) {
         if (word == null)
             return null;
         Deque<String> deque = new ArrayDeque<String>();
@@ -10,10 +10,6 @@ public class Palindrome {
             deque.addLast(String.valueOf(word.charAt(i)));
         }
         return deque;
-    }
-
-    private static boolean isPalindrome(Deque<String> deque) {
-        return false;
     }
 
     public boolean isPalindrome(String word) {
@@ -28,15 +24,25 @@ public class Palindrome {
         return true;
     }
 
+    private Deque<Character> wordToCharDeque(String word) {
+        if (word == null)
+            return null;
+        Deque<Character> deque = new ArrayDeque<Character>();
+        for(int i = 0; i < word.length(); ++i)
+        {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
     public boolean isPalindrome(String word, CharacterComparator cc) {
         if (word.length() < 2) {
             return true;
         }
-        int j = word.length() - 1;
-        for(int i = 0; j - i > 0; ++i, --j)
-        {
-            if(!cc.equalChars(word.charAt(i), word.charAt(j))) {
-                 return false;
+        Deque<Character> deque = wordToCharDeque(word);
+        while(deque.size() > 1){
+            if (!cc.equalChars(deque.removeFirst(),deque.removeLast())) {
+                return false;
             }
         }
         return true;
